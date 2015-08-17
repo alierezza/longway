7017c73f87add7b9031be5e02dc7528f92606e6c
hendranata <hendranatas@yahoo.com>
2015-08-17 20:07:02 +0700

    asd

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ require 'date'
 
   EM.run do
     @clients = []
-    EM::WebSocket.start(:host => 'localhost', :port => '3005') do |ws|
+    EM::WebSocket.start(:host => '192.168.100.25', :port => '3005') do |ws|
       ws.onopen do |handshake|
         @clients << ws
         ws.send "Conecting.."
