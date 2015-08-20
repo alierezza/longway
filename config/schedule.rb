637cd77b17d2ea3223daf07d91683af29118bcc8
hendranata <hendranatas@yahoo.com>
2015-08-20 20:36:11 +0700

    ifix

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,4 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
 set :output, "log/cron_log.log"
 
 every 1.day, :at => "6 pm" do
@@ -14,9 +8,5 @@ end
 every '0 7,8,9,10,11,12,13,14,15,16,17,18 * * *' do
   runner "Report.hourly"
 end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
 
-# Learn more: http://github.com/javan/whenever
+#@reboot sleep 5 && 'ruby /home/gloobalway/public_html/server.rb >> log/server.log 2>&1'
