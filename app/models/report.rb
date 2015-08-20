e22f32c1f8995cdb0eed49f680bcb3661b9882bf
hendranata <hendranatas@yahoo.com>
2015-08-20 10:04:55 +0700

    fsiaddy

--- app/models/report.rb
+++ app/models/report.rb
@@ -9,14 +9,23 @@ class Report < ActiveRecord::Base
 
 	
 	def self.hourly
-		User.where("status = true").each_with_index do |user,index|
-			opr = user.line.reports.last.detailreports.last.opr
-			remark = user.line.reports.last.detailreports.last.remark
-			percent = user.line.reports.last.detailreports.last.percent
-			act_sum = user.line.reports.last.detailreports.sum("act").to_i
-			pph = opr == 0 ? 0 : (act_sum / opr  .to_f * (user.line.reports.last.detailreports.count+1) ).round(2)
-			rft = user.line.reports.last.detailreports.last.rft
-			user.line.reports.last.detailreports.create!(:jam=>params[:record], :opr=>opr,:percent=>percent,:pph=>pph,:rft=>rft, :remark=>remark)
+		User.where("status = ? and role = ?",true,"User").each_with_index do |user,index|
+			begin
+				if user.line.reports.last.detailreports.last.jam.to_i == Time.now.strftime("%H").to_i
+					puts "Sudah ada!(user aktif) user: #{user.email}, waktu: #{Time.now.strftime("%d %m %Y %H:%M:%S")}"
+				else
+					opr = user.line.reports.last.detailreports.last.opr
+					remark = user.line.reports.last.detailreports.last.remark
+					percent = user.line.reports.last.detailreports.last.percent
+					act_sum = user.line.reports.last.detailreports.sum("act").to_i
+					pph = opr == 0 ? 0 : (act_sum / opr  .to_f * (user.line.reports.last.detailreports.count+1) ).round(2)
+					rft = user.line.reports.last.detailreports.last.rft
+					user.line.reports.last.detailreports.create!(:jam=>Time.now.strftime("%H").to_i, :opr=>opr,:percent=>percent,:pph=>pph,:rft=>rft, :remark=>remark)
+					puts "Sukses ! user: #{user.email}, waktu: #{Time.now.strftime("%d %m %Y %H:%M:%S")}"
+				end
+			rescue Exception => e
+				puts "errornya: #{e.message}, user: #{user.email}"
+			end
 		end
 	end
 
