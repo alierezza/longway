9569ced59f2c3808dff46f581ef446704eafbe47
hendranata <hendranatas@yahoo.com>
2016-04-18 09:33:47 +0700

    fiasd

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,17 +2,17 @@
 #set :output, "log/cron_log.log"
 
 every 1.day, :at => "6:10 pm" do
-  runner "Board.send_email >> log/cron_log.log 2>&1"
+  runner "Board.send_email", :output=>"log/send_email_log.log"
 end
 
 every '* 7,8,9,10,11,12,13,14,15,16,17,18 * * 1-6' do
-  runner "Report.hourly >> log/cron_log.log 2>&1"
+  runner "Report.hourly", :output=>"log/report_hourly_log.log"
 end
 
 every :friday, :at => "11.00 pm" do
-	runner "Board.remove >> log/cron_log.log 2>&1"	
+	runner "Board.remove", :output=>"log/remove_log.log"	
 end
 
 every :reboot do # Many shortcuts available: :hour, :day, :month, :year, :reboot
-  command "sleep 5 && ruby /var/www/visualboard/current/server.rb >> /var/www/visualboard/current/log/server.log 2>&1"
+  command "sleep 5 && ruby /var/www/visualboard/current/server.rb", :output=>"/var/www/visualboard/current/log/server.log"
 end
