3f66934c565587adabcf4f09046e199a61911f5f
hendranata <hendranatas@yahoo.com>
2016-05-18 09:22:13 +0700

    fix effisiensi

--- app/models/masteremail.rb
+++ app/models/masteremail.rb
@@ -22,7 +22,7 @@ class Masteremail < ActiveRecord::Base
 	    	else
 	    		board.reports.where("tanggal=?",tanggal).all.each_with_index do |report,index2|
 
-	    			sheet1.row(baris = baris+1).replace ["HOUR","OPR","TARGET","TARGET (SUM)", "ACT", "ACT (SUM)", "%", "PPH", "DEFECT","","","","","","","","","","","","","", "RFT", "REMARK", "ARTICLE","EFFICIENCY (Accumulation)", "P/O", "MFG No"]
+	    			sheet1.row(baris = baris+1).replace ["HOUR","OPR","TARGET","TARGET (SUM)", "ACT", "ACT (SUM)", "%", "PPH", "ARTICLE","EFFICIENCY (Accumulation)", "DEFECT","","","","","","","","","","","","","", "RFT", "REMARK", "P/O", "MFG No"]
 	    			sheet1.row(baris).height = 16
 	    			row = sheet1.row(baris)
 	    			format = Spreadsheet::Format.new :color => :black,
@@ -35,39 +35,39 @@ class Masteremail < ActiveRecord::Base
 					sheet1.column(4).width = 10
 					sheet1.column(5).width = 20
 					sheet1.column(6).width = 10
-					sheet1.column(7).width = 10
-					sheet1.column(8).width = 10
-					sheet1.column(9).width = 10
+					sheet1.column(7).width = 10 #pph
+					sheet1.column(8).width = 70 #article
+					sheet1.column(9).width = 30 #effisiensi (akumulasi)
 					sheet1.column(10).width = 10
 					sheet1.column(11).width = 10
 					sheet1.column(12).width = 10
 					sheet1.column(13).width = 10
-					sheet1.column(14).width = 15 #int(sum)
+					sheet1.column(14).width = 10
 					sheet1.column(15).width = 10
-					sheet1.column(16).width = 10
+					sheet1.column(16).width = 15 #int(sum)
 					sheet1.column(17).width = 10
 					sheet1.column(18).width = 10
 					sheet1.column(19).width = 10
 					sheet1.column(20).width = 10
-					sheet1.column(21).width = 15 #ext(sum)
+					sheet1.column(21).width = 10
 					sheet1.column(22).width = 10
-					sheet1.column(23).width = 70
-					sheet1.column(24).width = 70 #article
-					sheet1.column(25).width = 30 #effisiensi (akumulasi)
+					sheet1.column(23).width = 15 #ext(sum)
+					sheet1.column(24).width = 10
+					sheet1.column(25).width = 70 #remark
 					sheet1.column(26).width = 20
 					sheet1.column(27).width = 20
-					sheet1.merge_cells(baris, 8, baris, 21)
-					8.times do |y|
+					sheet1.merge_cells(baris, 10, baris, 23)
+					10.times do |y|
 						sheet1.merge_cells(baris, y, baris+1, y)
 					end
-					sheet1.merge_cells(baris, 22, baris+1, 22)
-					sheet1.merge_cells(baris, 23, baris+1, 23)
 					sheet1.merge_cells(baris, 24, baris+1, 24)
 					sheet1.merge_cells(baris, 25, baris+1, 25)
 					sheet1.merge_cells(baris, 26, baris+1, 26)
 					sheet1.merge_cells(baris, 27, baris+1, 27)
+					sheet1.merge_cells(baris, 28, baris+1, 28)
+					sheet1.merge_cells(baris, 29, baris+1, 29)
 	    			
-	    			sheet1.row(baris = baris+1).replace ["","","","","","","","","11A","11B","11C","11J","11L","13D","INT (SUM)","BS2","BS3","BS7","BS13","BS15","BS17","EXT (SUM)"]
+	    			sheet1.row(baris = baris+1).replace ["","","","","","","","","","","11A","11B","11C","11J","11L","13D","INT (SUM)","BS2","BS3","BS7","BS13","BS15","BS17","EXT (SUM)"]
 	    			sheet1.row(baris).height = 16
 	    			row = sheet1.row(baris)
 	    			28.times do |x|
@@ -95,7 +95,7 @@ class Masteremail < ActiveRecord::Base
 						# article
 						article_detail = ""
 						if detailreport.detailreportarticles != []
-							article_detail = detailreport.detailreportarticles.map{|i| i.article.to_s + "(act:" + i.output.to_s + ")" + " (opt:" + i.operator.to_s + ")" + " (work:" + ( (i.updated_at - i.created_at) / 1.minute ).ceil.to_s + " min)" }.join(", ")
+							article_detail = detailreport.detailreportarticles.map{|i| i.article.to_s + "(act:" + i.output.to_s + ")" + " (opt:" + i.operator.to_s + ")" }.join(", ")
 						end
 
 
@@ -106,7 +106,7 @@ class Masteremail < ActiveRecord::Base
 								
 								if Article.find_by_name(data[0]) != nil 
 									article = Article.find_by_name(data[0])
-									minutes = ( (data[4] - data[3]) / 1.minute ).ceil
+									minutes = 60
 									@article_x_duration.push(  data[2] * article.duration )
 									@total_working_time.push(  minutes )
 								else
@@ -122,7 +122,7 @@ class Masteremail < ActiveRecord::Base
 							efisiensi_akumulasi = " (#{ ((@article_x_duration.sum / ( @total_working_time.sum * detailreport.opr ) ) * 100) .round(2) }%)"
 						end
 
-						sheet1.row(baris = baris+1).replace [detailreport.jam,detailreport.opr,detailreport.target,sum_target,detailreport.act,sum_act,detailreport.percent.to_i,detailreport.pph,detailreport.defect_int,detailreport.defect_int_11b,detailreport.defect_int_11c,detailreport.defect_int_11j,detailreport.defect_int_11l,detailreport.defect_int_13d,sum_defect_int,detailreport.defect_ext,detailreport.defect_ext_bs3,detailreport.defect_ext_bs7,detailreport.defect_ext_bs13,detailreport.defect_ext_bs15,detailreport.defect_ext_bs17,sum_defect_ext,detailreport.rft.to_i,detailreport.remark == nil ? nil : detailreport.remark.gsub(/\n/, ' ').gsub(/\r/,' '), article_detail.html_safe , efisiensi_akumulasi.html_safe , detailreport.po, detailreport.mfg]
+						sheet1.row(baris = baris+1).replace [detailreport.jam,detailreport.opr,detailreport.target,sum_target,detailreport.act,sum_act,detailreport.percent.to_i,detailreport.pph, article_detail.html_safe , efisiensi_akumulasi.html_safe ,detailreport.defect_int,detailreport.defect_int_11b,detailreport.defect_int_11c,detailreport.defect_int_11j,detailreport.defect_int_11l,detailreport.defect_int_13d,sum_defect_int,detailreport.defect_ext,detailreport.defect_ext_bs3,detailreport.defect_ext_bs7,detailreport.defect_ext_bs13,detailreport.defect_ext_bs15,detailreport.defect_ext_bs17,sum_defect_ext,detailreport.rft.to_i,detailreport.remark == nil ? nil : detailreport.remark.gsub(/\n/, ' ').gsub(/\r/,' '), detailreport.po, detailreport.mfg]
 						sheet1.row(baris).height = height * 16
 						row = sheet1.row(baris)
 
