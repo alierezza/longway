f4255599be30b3e9e321084b93d4f137b397272d
Shoerofi <rofi.shoerofi@gmail.com>
2016-06-23 22:46:07 +0700

    fix duplication code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,4 +44,86 @@ module ApplicationHelper
 	def sum_defect_int(detailreport)
 		JSON.parse(detailreport.object.defect_int.to_s).map{ |k,v| v }.sum
 	end
+
+	def show_defect_header(data, type)
+		html = ""
+		defect = Defect.where(defect_type: type)
+
+		if type == "Internal"
+			head = "INT"
+		else
+			head = "EXT"
+		end
+
+		if data.present?
+			data.each_with_index do |(key, val), index|
+				html += "<th>#{head + ' ' + key}</th>"
+				if (index+1) == data.map{ |k,v| k }.length
+					html += "<th>#{head} (SUM)</th>"
+				end
+			end
+		else
+			defect.each_with_index do |defect, index|
+				html += "<th>#{head + ' ' + defect.name}</th>"
+				if (index+1) == defect.length
+					html += "<th>#{head} (SUM)</th>"
+				end
+			end
+		end
+
+		html.html_safe
+	end
+
+	def show_defect_body(detailreport, type, alternative)
+		html = ""
+		working_state = WorkingDay.find_by_name(detailreport.report.tanggal.strftime("%A")).working_hours.find_by_start(detailreport.jam).working_state
+		defect = Defect.where(defect_type: type)
+
+		if type == "Internal"
+			data = [detailreport.defect_int, "total_defect_int"]
+		else
+			data = [detailreport.defect_ext, "total_defect_ext"]
+		end
+
+		if JSON.parse(data[0]).present?
+			JSON.parse(data[0]).each_with_index do |(key, val), index|
+				html += "<td>#{val}</td>"
+				if (index+1) == JSON.parse(data[0]).map{ |k,v| k }.length
+					html += "<td>#{Report.send data[1], detailreport.report, detailreport.jam}</td>"
+				end
+			end
+		elsif working_state == "Break"
+			if alternative.present?
+				alternative.each_with_index do |(key, val), index|
+					html += "<td>-</td>"
+					if (index+1) == alternative.map{ |k,v| k }.length
+						html += "<td>-</td>"
+					end
+				end
+			else
+				defect.each_with_index do |defect, index|
+					html += "<td>-</td>"
+					if (index+1) == defect.length
+						html += "<td>-</td>"
+					end
+				end
+			end
+		elsif alternative.present?
+			alternative.each_with_index do |(key, val), index|
+				html += "<td>0</td>"
+				if (index+1) == alternative.map{ |k,v| k }.length
+					html += "<td>#{Report.send data[1], detailreport.report, detailreport.jam}</td>"
+				end
+			end
+		else
+			defect.each_with_index do |defect, index|
+				html += "<td>#{val}</td>"
+				if (index+1) == defect.length
+					html += "<td>#{Report.send data[1], detailreport.report, detailreport.jam}</td>"
+				end
+			end
+		end
+
+		html.html_safe
+	end
 end
