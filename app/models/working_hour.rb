6e0046d6207e292c1e33f8226b8526d3e2bdc633
Shoerofi <rofi.shoerofi@gmail.com>
2016-06-23 11:32:12 +0700

    fix validation working hour

--- app/models/working_hour.rb
+++ app/models/working_hour.rb
@@ -4,7 +4,7 @@ class WorkingHour < ActiveRecord::Base
   belongs_to :working_day
 
   validates_uniqueness_of :start, :scope => [:end,:working_day_id]
-  validate :check_validation, :duration, :start_must_be_before_end_time, :end_must_be_before_start_time
+  validate :check_validation, :duration, :start_must_be_before_end_time
 
   def self.state
   	["Work", "Break", "Overtime"]
@@ -13,15 +13,18 @@ class WorkingHour < ActiveRecord::Base
   def check_validation
     self.working_day.working_hours.where.not(id: nil).each do |hour|
     	if self.start.to_time.between?(hour.start.to_time+1, hour.end.to_time-1)
-    		errors.add(:base, 'Wrong start and end time 1.')
+    		errors.add(:base, 'Start should not be between another scheduled time.')
     		break
-    	end
+    	elsif self.end.to_time.between?(hour.start.to_time+1, hour.end.to_time-1)
+        errors.add(:base, 'End should not be between another scheduled time.')
+        break
+      end
     end
 	end
 
 	def start_must_be_before_end_time
 		if self.end.to_time <= self.start.to_time
-			errors.add(:base, 'Wrong start and end time 2.')
+			errors.add(:base, 'Wrong time.')
 		end
 	end
 
@@ -30,15 +33,5 @@ class WorkingHour < ActiveRecord::Base
 			((self.end.to_time - self.start.to_time) / 60 ).to_i < 30
 			errors.add(:base, 'Time duration should be 30 - 60 minutes.')
 		end
-	end
-
-	def end_must_be_before_start_time
-		self.working_day.working_hours.where.not(id: nil).each do |hour|
-      binding.pry
-    	if self.end.to_time.between?(hour.start.to_time+1, hour.end.to_time-1)
-    		errors.add(:base, 'Wrong start and end time 3.')
-    		break
-    	end
-    end
 	end
 end
