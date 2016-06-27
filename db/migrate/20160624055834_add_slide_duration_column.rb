673f884c5b605bf8ff0b95fb5e9445c2a74eb4f0
hendranata <hendranatas@yahoo.com>
2016-06-27 18:30:22 +0700

    fix default

--- db/migrate/20160624055834_add_slide_duration_column.rb
+++ db/migrate/20160624055834_add_slide_duration_column.rb
@@ -1,6 +1,6 @@
 class AddSlideDurationColumn < ActiveRecord::Migration
   def change
-  	add_column :lines, :slide_duration, :integer, default: 5
-  	add_column :images, :slide_duration, :integer, default: 5
+  	add_column :lines, :slide_duration, :integer, default: 15
+  	add_column :images, :slide_duration, :integer, default: 15
   end
 end
