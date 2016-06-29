7986c0c005bb85a0c4f1dcd81046cf58c653057c
hendranata <hendranatas@yahoo.com>
2016-06-29 09:13:33 +0700

    change country & category null true

--- db/migrate/20160615080932_add_categoryandcountry_to_detailreport.rb
+++ db/migrate/20160615080932_add_categoryandcountry_to_detailreport.rb
@@ -1,6 +1,6 @@
 class AddCategoryandcountryToDetailreport < ActiveRecord::Migration
   def change
-  	add_column :detailreports, :country, :string, null: false
-  	add_column :detailreports, :category, :string, null: false
+  	add_column :detailreports, :country, :string
+  	add_column :detailreports, :category, :string
   end
 end
