835bb89f32a0626587770da8d6dcbaa1801b62a5
hendranata <hendranatas@yahoo.com>
2016-06-29 18:33:57 +0700

    fix deploy globalway gloves

--- config/deploy/globalwayprotection_id.rb
+++ config/deploy/globalwayprotection_id.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'localhost', user: 'globalway', roles: %w{app db web}, my_property: :my_value, password: 'q1w2e3r4'
+server 'localhost', user: 'globalway', roles: %w{app db web}, my_property: :my_value, password: 'globalway'
 
 set :deploy_to, '/var/www/longway'
 
