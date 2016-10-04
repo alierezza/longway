58e74c02fd2e151ef3cce673df8d2e9280d38d67
hendranata <hendranatas@yahoo.com>
2016-10-04 07:37:18 +0700

    fix xls

--- app/models/masteremail.rb
+++ app/models/masteremail.rb
@@ -8,9 +8,9 @@ class Masteremail < ActiveRecord::Base
 
 	    book = Spreadsheet::Workbook.new
 	    sheet1 = book.create_worksheet
-	    sheet1.name = Language.find_by(:message=>"Company Title").foreign_language+' Daily Report'
+	    sheet1.name = ActionView::Base.full_sanitizer.sanitize(Language.find_by(:message=>"Company Title").foreign_language)+' Daily Report'
 
-	    sheet1.row(0).push(Language.find_by(:message=>"Company Title").foreign_language+" - Production result on #{tanggal.to_date.strftime('%d %B %Y')} taken at #{Time.now.strftime('%H:%M')}")
+	    sheet1.row(0).push(ActionView::Base.full_sanitizer.sanitize(Language.find_by(:message=>"Company Title").foreign_language)+" - Production result on #{tanggal.to_date.strftime('%d %B %Y')} taken at #{Time.now.strftime('%H:%M')}")
 	    baris = 0
 	    total_length = 0
 	    Line.all.where("visible=?",true).order("no").each_with_index do |board,index|
