c7aa65be80d3387aef35575a2b9ef50fed12c6dd
hendranata <hendranatas@yahoo.com>
2016-11-17 12:16:17 +0700

    fix

--- app/controllers/emailsettings_controller.rb
+++ app/controllers/emailsettings_controller.rb
@@ -9,26 +9,26 @@ class EmailsettingsController < ApplicationController
 	def update
 		
 
-		begin
-			ActiveRecord::Base.transaction do
+		#begin
+		#	ActiveRecord::Base.transaction do
 
-				@emailsetting = Emailsetting.find(Emailsetting.first.id)
-				if @emailsetting.update(:email_time=>params[:emailsetting][:email_time])
-
-					flash[:notice] = "Update success"
-					redirect_to edit_emailsetting_path(Emailsetting.first,:set=>true)
-				else
-					flash[:alert] = "Update failed"
-					redirect_to edit_emailsetting_path(Emailsetting.first)
-				end
-
-				
-
-			end
-		rescue
+		@emailsetting = Emailsetting.find(Emailsetting.first.id)
+		if @emailsetting.update(:email_time=>params[:emailsetting][:email_time])
+			 %x[ whenever --update-crontab ]
+			flash[:notice] = "Update success"
+			redirect_to edit_emailsetting_path(Emailsetting.first)
+		else
 			flash[:alert] = "Update failed"
 			redirect_to edit_emailsetting_path(Emailsetting.first)
 		end
+
+				
+
+			#end
+		# rescue
+		# 	flash[:alert] = "Update failed"
+		# 	redirect_to edit_emailsetting_path(Emailsetting.first)
+		# end
 
 
 	end
