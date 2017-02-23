7feac7e95a1c465fd89692822219e85899e091b9
hendranata <hendranatas@yahoo.com>
2017-02-23 09:22:53 +0700

    fixy

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,8 @@ class Article < ActiveRecord::Base
 	validates :name, :presence=>true, :uniqueness=>true
 	validates :duration, :presence=>true
 
-	
+
+	def name=(val)
+	    write_attribute :name, val.upcase
+	end
 end
